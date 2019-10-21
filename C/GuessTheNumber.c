5dd1bb00ca32f971c2043f23b978d648e29c1634
Sebastian <gawron241@gmail.com>
2019-10-21 19:42:21 +0200

    Poprawione bledy i dodany counter Losuje od 1 do 100 i liczy Twoje proby

--- C/GuessTheNumber.c
+++ C/GuessTheNumber.c
@@ -6,8 +6,8 @@ int main()
 {
     srand(time(NULL));
 
-    int n = rand() % 100;
-
+    int n = rand() % 101;
+    int counter = 0;
     char w = 0;
 
     do
@@ -19,17 +19,20 @@ int main()
         if (l > n)
         {
             printf("Za duzo!\n");
+	    counter++;
         }
         else if (l < n)
         {
             printf("Za malo!\n");
+	    counter++;
         }
         else
         {
             printf("Wygrales!\n");
+	    printf("Udalo Ci sie za proba numer: %d", counter)
             w = 1;
-        }
-
+        }	
     }
+
     while (w == 0);
 }
